b3b95b1a463ea3d815aac7d3803822ea353d3c9e
Gabriel Lantin <luigilantin@gmail.com>
2021-09-27 20:05:03 -0700

    added todos

---
+++ script.js
@@ -0,0 +1,12 @@
+// TODO:
+// timer
+// questions - obj
+// questions answered - int
+// score - int
+
+// start button -> timer starts (upper left), first question
+// correct answer -> display message/wrong answer -> display message + subtract time
+// game over when all questions are answered or timer reaches 0
+// save initials and score -> submit, taken to highscores page
+
+// highscores page: show all highscores, go back and clear highscores buttons
